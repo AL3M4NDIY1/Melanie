fcc60c37725d504c097c4f077b671cb15b0d064c
AL3M4NDIY1 <hugosebastianvillalva14@gmail.com>
2024-09-14 02:42:48 -0300

    first commit

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function validarFecha() {
         const mes = hoy.getMonth() + 1;
     
         // Usar == o === para comparar valores
-        if (dia == 21 && mes == 9) {
+        if (dia == 14 && mes == 9) {
             // Asignar la URL a window.location.href
             window.location.href = 'FLORES.html';
         }
