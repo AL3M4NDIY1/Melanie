6a12e909761eb6fce94cea63f1f78466b756aafe
AL3M4NDIY1 <hugosebastianvillalva14@gmail.com>
2024-09-14 02:39:44 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+onload = () =>{
+        document.body.classList.remove("container");
+};
+
+
+function validarFecha() {
+        const hoy = new Date();
+        const dia = hoy.getDate();
+        const mes = hoy.getMonth() + 1;
+    
+        // Usar == o === para comparar valores
+        if (dia == 21 && mes == 9) {
+            // Asignar la URL a window.location.href
+            window.location.href = 'FLORES.html';
+        }
+    }
+    
